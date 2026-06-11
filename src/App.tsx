dbf865245ca4ce2f968b98ff388e7b56640d62ed
Saga Engström <Saga97sle@hotmail.com>
2026-06-11 11:34:55 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import HomePage from "./pages/home/HomePage";
-import RoomPage from "./pages/roomPage/RoomPage";
-import VictoryPage from "./pages/victoryPage/VictoryPage";
+import RoomPage from "./pages/room/RoomPage";
+import VictoryPage from "./pages/victory/VictoryPage";
 
 const App = () => {
   return <div></div>;
