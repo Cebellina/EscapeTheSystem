62f1356cf968e08e62bb1048d2ce105caba7bbef
Cebellina <emma.andersson.se@outlook.com>
2026-06-12 14:08:22 +0200

    add replace to Navigate redirects

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,20 @@ import Navbar from "./components/Navbar";
 import HomePage from "./pages/home/HomePage";
 import RoomPage from "./pages/room/RoomPage";
 import VictoryPage from "./pages/victory/VictoryPage";
+import { InventoryProvider } from "./context/InventoryContext";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/room/:roomPath" element={<RoomPage />} />
-        <Route path="/victory" element={<VictoryPage />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <InventoryProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/room/:roomPath" element={<RoomPage />} />
+          <Route path="/victory" element={<VictoryPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </InventoryProvider>
     </BrowserRouter>
   );
 };
