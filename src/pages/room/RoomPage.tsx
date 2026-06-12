23146cfa758d870be3767e88711d8c455ce920a3
Saga Engström <Saga97sle@hotmail.com>
2026-06-12 09:43:29 +0200

    added unsolved image

--- src/pages/room/RoomPage.tsx
+++ src/pages/room/RoomPage.tsx
@@ -1,5 +1,15 @@
+import { useParams } from "react-router-dom";
+import rooms from "../../data/rooms.json";
+
 const RoomPage = () => {
-  return <div></div>;
+  const { roomPath } = useParams();
+  const room = rooms.find((r) => r.roomPath === roomPath);
+
+  return (
+    <div>
+      <img src={room?.unsolvedImage} />
+    </div>
+  );
 };
 
 export default RoomPage;
